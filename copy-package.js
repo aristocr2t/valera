d5a0a059d051176b89e7c5526672c4a81709b8fd
Shamil Sultanov <shamil.amirovich@gmail.com>
2020-06-11 20:44:32 +0300

    fix bugs

--- copy-package.js
+++
@@ -1,9 +0,0 @@
-#!/bin/node
-
-const fs = require('fs');
-const packageJson = JSON.parse(fs.readFileSync('package.json').toString());
-
-delete packageJson.scripts;
-delete packageJson.devDependencies;
-
-fs.writeFileSync('dist/package.json', JSON.stringify(packageJson, null, 2));
